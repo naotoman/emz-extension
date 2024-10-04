042adbe95d605a25b88cef10dd4027e54ae36792
naotoman <37291085+naotoman@users.noreply.github.com>
2024-10-05 00:33:18 +0900

    pass token

---
+++ src/content-scripts/scriptSystem.ts
@@ -0,0 +1,30 @@
+import { waitQuerySelectorAll } from "../common/utils";
+
+const getLocalStorageValueByRegex = (regex: RegExp) => {
+  const keys = Object.keys(localStorage);
+  const key = keys.find((key) => key.match(regex));
+  return key ? localStorage.getItem(key) : null;
+};
+
+const observer = new MutationObserver(() => {
+  setTimeout(() => {
+    const idToken = getLocalStorageValueByRegex(
+      /^CognitoIdentityServiceProvider\..+\.idToken$/
+    );
+    const refreshToken = getLocalStorageValueByRegex(
+      /^CognitoIdentityServiceProvider\..+\.refreshToken$/
+    );
+    console.log({ idToken, refreshToken });
+    if (idToken && refreshToken) {
+      chrome.storage.local.set({
+        auth: { idToken, refreshToken },
+      });
+    }
+  }, 500);
+});
+
+waitQuerySelectorAll("#root").then((root) => {
+  observer.observe(root[0], {
+    childList: true,
+  });
+});
